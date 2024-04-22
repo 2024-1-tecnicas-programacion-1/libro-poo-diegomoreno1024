c0da060e91c578712e48f2c18badd5ffdadb55b4
Andrès Patarroyo <A@gmail.com>
2024-04-22 10:25:21 -0500

    Se soluciona el ejercicio.

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,48 @@
 package com.mycompany.libropoo;
 
+import java.time.LocalDate;
+
 public class Libro {
-    
+        
+    private String titulo;
+    private String autor;
+    private int anioPublicacion;
+    private boolean leido=false ;
+    private String genero;
+    //Este constructor tiene una complejidad constante, O(1).
+    public Libro(String titulo,String autor,int anioPublicacion,String genero){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.anioPublicacion=anioPublicacion;
+        this.genero=genero;
+    }
+    //La complejidad de este método es constante, O(1).
+    public String mostrarInformacion(){
+        return "Título: "+this.titulo+",Autor: "+this.autor+",Año de publicación: "+this.anioPublicacion+",Género: "+this.genero;
+    }
+    //La complejidad de este método es constante, O(1).
+    public void marcarLeido(){
+        this.leido=true;
+    }
+    //
+    public String getLeido(){
+        if(this.leido==true){
+            return "El libro fue leido.";
+        }else
+            return "El libro no fue leido";
+    }
+    //La complejidad de este método es constante, O(1).
+    public boolean esAntiguo(){
+        int anioActual=LocalDate.now().getYear();
+        if(this.anioPublicacion>=0){
+            if(anioActual-this.anioPublicacion>50){
+                return true;
+            }else if(this.anioPublicacion-anioActual<50){
+                return false;
+            }
+        }else if(this.anioPublicacion<0){
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,34 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+import java.time.LocalDate;
+
 public class Principal {
     
+     public static void main(String[] args) {
+         Scanner leer= new Scanner(System.in);
+         
+         System.out.println("Ingrese el título del libro");
+         String titulo=leer.nextLine();
+         System.out.println("Ingrese el autor del libro");
+         String autor=leer.nextLine();
+         System.out.println("Ingrese el género del libro");
+         String genero= leer.nextLine();
+         System.out.println("Ingrese el año de publicación del libro");
+         int anioPublicacion=leer.nextInt();
+         if(anioPublicacion>LocalDate.now().getYear()){
+             System.out.println("Año invalido");
+         }else{         
+             Libro libroIngresado = new Libro(titulo,autor,anioPublicacion,genero);
+             libroIngresado.marcarLeido();
+             System.out.println(libroIngresado.mostrarInformacion());
+             System.out.println(libroIngresado.getLeido());
+             if(libroIngresado.esAntiguo()==true){
+                 System.out.println("El libro es antiguo.");
+             }else
+                 System.out.println("El libro no es antiguo.");
+            }
+    }
 }
+
+
